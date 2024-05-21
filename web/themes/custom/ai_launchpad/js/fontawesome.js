c805dc508a89d6357f20fb3eb4f96617acd2aef6
Jesper Pedersen <jesper@yepz.dk>
2024-05-21 14:46:22 +0200

    1434: Update coding standards

--- web/themes/custom/ai_launchpad/js/fontawesome.js
+++ web/themes/custom/ai_launchpad/js/fontawesome.js
@@ -1,14 +1,11 @@
 /* // Add fontawesome icons */
 
 // Import the svg core
-const { library, dom } = require('@fortawesome/fontawesome-svg-core');
+const { library, dom } = require("@fortawesome/fontawesome-svg-core");
 
 // To keep the package size as small as possible we only import icons we use
 // Import the icons from the free solid package.
-const {
-  faBars,
-  faCircle,
-} = require('@fortawesome/free-solid-svg-icons');
+const { faBars, faCircle } = require("@fortawesome/free-solid-svg-icons");
 
 // Import icons from the free brands package
 // const {
@@ -19,14 +16,14 @@ const {
 library.add(
   // Solid
   faBars,
-  faCircle
+  faCircle,
   // Brand
   // faXTwitter
 );
 
 // Run <i> to <svg> replace
 function iconsDoneRendering() {
-  console.log('Icons have rendered')
+  console.log("Icons have rendered");
 }
 
-dom.i2svg({ callback: iconsDoneRendering })
+dom.i2svg({ callback: iconsDoneRendering });
